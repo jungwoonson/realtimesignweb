1608b70dd9e1810e7ef12f0ca21974e19812d803
jungwoonson <jungwoon727@gmail.com>
2024-03-11 23:14:42 +0900

    feat: MvcConfiguration 추가

---
+++ src/main/java/com/toy/boardserver/realtimesignweb/sign/SignController.java
@@ -0,0 +1,14 @@
+package com.toy.boardserver.realtimesignweb.sign;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SignController {
+
+    @GetMapping
+    public String index() {
+        return "index.html";
+    }
+
+}
