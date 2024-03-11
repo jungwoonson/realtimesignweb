25c1a6e2ae232b9c5d34aff8de3fef0f248adb5f
jungwoonson <jungwoon727@gmail.com>
2024-03-11 18:00:35 +0900

    feat: 프로젝트 초기화

---
+++ src/main/java/com/toy/boardserver/realtimesignweb/RealtimesignwebApplication.java
@@ -0,0 +1,13 @@
+package com.toy.boardserver.realtimesignweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimesignwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimesignwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy/boardserver/realtimesignweb/RealtimesignwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.boardserver.realtimesignweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimesignwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
